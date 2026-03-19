6e1cdd71bf2c731432f4a5eaba892f09ef0d7e92
María Almendras <almendrasmaria04@gmail.com>
2026-03-19 11:25:24 -0300

    feat(auth): add auth pages structure

---
+++ apps/web/src/modules/auth/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginCard from "../components/LoginCard";
+
+const LoginPage = () => {
+  return (
+    <main className="h-screen w-screen overflow-hidden bg-[#f3f4f6]">
+      <LoginCard />
+    </main>
+  );
+};
+
+export default LoginPage;
---
+++ apps/web/src/modules/auth/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import RegisterCard from "../components/RegisterCard";
+
+const RegisterPage = () => {
+  return (
+    <main className="h-screen w-screen overflow-hidden bg-[#f3f4f6]">
+      <RegisterCard />
+    </main>
+  );
+};
+
+export default RegisterPage;
